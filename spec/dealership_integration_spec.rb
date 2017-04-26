9286a732dbb14e7cf182216290702bdd042a7c1f
Ben Williams and Mohamed Warsame <mohamed@>
2017-04-26 15:05:33 -0700

    successfully pass integration test.

--- spec/dealership_integration_spec.rb
+++ spec/dealership_integration_spec.rb
@@ -4,12 +4,11 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-# describe('paths for dealership', {:type => :feature}) do
-#   it('adds a dealership') do
-#     visit('/dealerships/new')
-#     fill_in('word1', :with => 'ruby')
-#     fill_in('word2', :with => 'bury')
-#     click_button('Go!')
-#     expect(page).to have_content('These words are anagrams but the first word is not a palindrome')
-#   end
-# end
+describe('paths for dealership', {:type => :feature}) do
+  it('adds a dealership') do
+    visit('/dealerships/new')
+    fill_in('name', :with => 'ruby')
+    click_button('Add Dealership')
+    expect(page).to have_content('Add')
+  end
+end
